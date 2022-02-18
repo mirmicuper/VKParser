cc57d8dba1689c74fd8308b2f7bfdb787abfd576
mirmicuper <alexander.onthe@gmail.com>
2022-02-19 01:35:01 +0300

    added main

--- parser.py
+++ parser.py
@@ -10,30 +10,16 @@ from email.mime.multipart import MIMEMultipart
 # const
 token = 'abbf5a7cabbf5a7cabbf5a7cbeabc44283aabbfabbf5a7cca4700031cbc96ae8f1e2bdd'
 version = 5.131
-#Изменяемый параметр user_id
-
-user_id = str(input("Введите ID: ")) #329743555 
-print("Expectation...")
 user_ids = '296213477'
 order = 'hints'
 fields ='sex, country, schools, bdate, last_seen, city, has_mobile'
 
-#write friends id of acc
-response = requests.get('https://api.vk.com/method/friends.get', params={
-	'access_token':token, 
-	'v':version, 
-	'user_id':user_id,
-	'order':order
-	})
-data = response.json()['response']['items']
-#create theader in csv-file
-with open('data.csv', 'w') as file:
-	a_pen = csv.writer(file)
-	a_pen.writerow(['id', 'Имя', 'Фамилия', 'День Рождения', 'Страна', 'Город'])
 
-
-#function write rows with data of friends 
 def getInfoInCSV(idUser, first_name, last_name, bdate, country, city):
+	with open('data.csv', 'w') as fileq:
+		aq_pen = csv.writer(fileq)
+		aq_pen.writerow(['id', 'Имя', 'Фамилия', 'День Рождения', 'Страна', 'Город'])
+
 	with open('data.csv', 'a') as file:
 		a_pen = csv.writer(file)
 		my_list = [idUser, first_name, last_name, bdate, country, city]
@@ -44,37 +30,30 @@ def getVariable(htmlp):
 		idUser = htmlp.json()['response'][0]['id']
 	except: 
 		idUser = "None"
-
 	try:
 		first_name = htmlp.json()['response'][0]['first_name']
 	except: 
 		first_name = "None"
-
 	try:
 		last_name = htmlp.json()['response'][0]['last_name']
 	except: 
 		last_name = "None"
-
 	try:
 		bdate = htmlp.json()['response'][0]['bdate']
 	except:
 		bdate = "None"
-
 	try:
 		sex = htmlp.json()['response'][0]['sex']
 	except: 
 		sex = "None"
-
 	try:
 		country = htmlp.json()['response'][0]['country']['title']
 	except: 
 		country =  "None"
-
 	try:
 		city = htmlp.json()['response'][0]['city']['title']
 	except: 
 		city = "None"
-
 	try:
 		last_seen_platform = htmlp.json()['response'][0]['last_seen']['platform']
 	except: 
@@ -84,12 +63,10 @@ def getVariable(htmlp):
 		value = datetime.datetime.fromtimestamp(last_seen_time)
 	except: 
 		value = "None"
-
 	try:
 		has_mobile = htmlp.json()['response'][0]['has_mobile']
 	except: 
 		has_mobile = "None"
-
 
 	if has_mobile == 0:
 		has_mobile = 'не найден'
@@ -103,8 +80,14 @@ def getVariable(htmlp):
 
 	getInfoInCSV(idUser, first_name, last_name, bdate, country, city)
 
-#general function (data about friends)
-def getInfo(data):
+def getInfo(user_id):
+	response = requests.get('https://api.vk.com/method/friends.get', params={
+		'access_token':token, 
+		'v':version, 
+		'user_id': user_id,
+		'order':order
+	})
+	data = response.json()['response']['items']
 	for i in data:
 	#write data about friend id
 		htmlp = requests.get('https://api.vk.com/method/users.get', params={
@@ -113,6 +96,7 @@ def getInfo(data):
 			'user_ids': str(i),
 			'fields':fields
 			})
+		print(i)
 		try:
 			getVariable(htmlp)
 		except Exception as e:
@@ -125,7 +109,6 @@ def getInfo(data):
 	# password  = "shatohin6701" 
 	# addr_to = "alexander.onthe@gmail.com"
 	# files = ["data.csv"]
-
 
 def send_email():
 	try:
@@ -157,7 +140,33 @@ def send_email():
 	except Exception as _ex:
 		return f"{_ex}\nCheck your login or password please!"                         
 
-getInfo(data)
-send_email()
+def getID(user_link):
+	user_id = user_link[17:]
+	if user_link == ('https://vk.com/id' + user_id):
+		return user_id
+	else:
+		idUserName = user_link[15:]
+		user_id = requests.get('https://api.vk.com/method/utils.resolveScreenName', params={
+			'access_token':token, 
+			'v':version, 
+			'screen_name': idUserName
+		})
+		user_id = user_id.json()["response"]["object_id"]
+		return user_id
+		
+def baner():
+	command_terminal = input()
+	if command_terminal == "send mail":
+		send_email()
+		return baner()
+	if command_terminal == "get info":
+		main()
+		return baner()
+	if command_terminal == "exit":
+		return
 
-
+def main():
+	user_link = input("link on user: ")
+	user_id = getID(user_link)
+	getInfo(user_id)
+baner()
